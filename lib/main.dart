78f5f5f9c8fd34c9b0bed9dfda1bb73017d84ae0
daniyal <mdaniyal9@bitbucket.org>
2020-11-10 16:25:00 +0500

    flatbutton added

--- lib/main.dart
+++ lib/main.dart
@@ -16,10 +16,12 @@ class Home extends StatelessWidget {
         backgroundColor: Colors.red[600],
       ),
       body: Center(
-        child: Icon(
-          Icons.airport_shuttle,
+        child: FlatButton(
+          onPressed: () {
+            print('you clicked me');
+          },
+          child: Text('click me'),
           color: Colors.lightBlue,
-          size: 50.0,
         ),
       ),
       floatingActionButton: FloatingActionButton(
