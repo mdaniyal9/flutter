ae2f5868903a6526176e474627aa647bb774c0e4
daniyal <mdaniyal9@bitbucket.org>
2020-11-11 16:10:57 +0500

    added routes to move from 1 activity to another

--- lib/main.dart
+++ lib/main.dart
@@ -1,47 +1,17 @@
 import 'package:flutter/material.dart';
-import 'quote.dart';
-import 'quote_card.dart';
+import 'package:flutter_app/pages/choose_location.dart';
+import 'package:flutter_app/pages/home.dart';
+import 'package:flutter_app/pages/loading.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: QuoteList(),
+    initialRoute: '/home',
+    routes: {
+      '/': (context) => Loading(),
+      '/home': (context) => Home(),
+      '/location': (context) => ChooseLocation(),
+    },
   ));
-}
-
-class QuoteList extends StatefulWidget {
-  @override
-  _QuoteListState createState() => _QuoteListState();
-}
-
-class _QuoteListState extends State<QuoteList> {
-
-  List<Quote> quotes = [
-    Quote(author: 'oscar Wild', text: 'Be yourself; everyone else is already taken'),
-    Quote(author: 'oscar Wild', text: 'I have nothing to declare except my genius'),
-    Quote(author: 'oscar Wild', text: 'The truth is rarely pure and never simple')
-  ];
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.grey[200],
-      appBar: AppBar(
-        title: Text('Awesome Quote'),
-        centerTitle: true,
-        backgroundColor: Colors.redAccent,
-      ),
-      body: Column(
-        children: quotes.map((quote) => QuoteCard(
-          quote: quote,
-          delete: () {
-            setState(() {
-              quotes.remove(quote);
-            });
-          }
-        )).toList(),
-      ),
-    );
-  }
 }
 
 
