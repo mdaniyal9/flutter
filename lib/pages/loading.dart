45f9b0f86c1577417a714eaac9d5c939360207c3
daniyal <mdaniyal9@bitbucket.org>
2020-11-12 09:23:55 +0500

    used an api call to get time and coverted it to jsonObject, then used substring, int.parse and Duration

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -9,17 +9,27 @@ class Loading extends StatefulWidget {
 
 class _LoadingState extends State<Loading> {
 
-  void getData() async {
-    Response response = await get('https://jsonplaceholder.typicode.com/todos/1');
+  void getTime() async {
+    Response response = await get('https://worldtimeapi.org/api/timezone/Asia/Karachi');
     Map data = jsonDecode(response.body);
-    print(data);
-    print(data['title']);
+    // print(data);
+
+    // get properties from data
+    String datetime = data['datetime'];
+    String offset = data['utc_offset'].substring(1,3);
+    // print(datetime);
+    // print(offset);
+
+    //create a datetime object
+    DateTime now = DateTime.parse(datetime);
+    now = now.add(Duration(hours: int.parse(offset)));
+    print(now);
   }
 
   @override
   void initState() {
     super.initState();
-    getData();
+    getTime();
   }
 
   @override
