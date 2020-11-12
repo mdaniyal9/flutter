b507fd9ac251b21b6b20c5133c940b36a9cc024f
daniyal <mdaniyal9@bitbucket.org>
2020-11-12 12:23:05 +0500

    used pop of navigator to return data to activity from it was opened, added a check in home to correctly display data

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -20,6 +20,19 @@ class _ChooseLocationState extends State<ChooseLocation> {
     WorldTime(url: 'Asia/Jakarta', location: 'Jakarta', flag: 'indonesia.png'),
   ];
 
+  void updateTime(index) async {
+    WorldTime instance = locations[index];
+    await instance.getTime();
+
+    // navigate to home screen
+    Navigator.pop(context, {
+      'location': instance.location,
+      'flag': instance.flag,
+      'time': instance.time,
+      'isDaytime': instance.isDaytime
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -37,7 +50,9 @@ class _ChooseLocationState extends State<ChooseLocation> {
             padding: EdgeInsets.symmetric(vertical: 1.0, horizontal: 4.0),
             child: Card(
               child: ListTile(
-                onTap: () {},
+                onTap: () {
+                  updateTime(index);
+                },
                 title: Text(locations[index].location),
                 leading: CircleAvatar(
                   backgroundImage: AssetImage('assets/${locations[index].flag}'),
