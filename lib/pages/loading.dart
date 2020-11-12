d1de38feb3932f4547c6233feaba3b4afd61172a
daniyal <mdaniyal9@bitbucket.org>
2020-11-12 09:56:11 +0500

    moved the api call to seperate class and used async and await in dart file to get time

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart';
-import 'dart:convert';
+import 'package:flutter_app/services/world_time.dart';
+
 
 class Loading extends StatefulWidget {
   @override
@@ -9,33 +9,30 @@ class Loading extends StatefulWidget {
 
 class _LoadingState extends State<Loading> {
 
-  void getTime() async {
-    Response response = await get('https://worldtimeapi.org/api/timezone/Asia/Karachi');
-    Map data = jsonDecode(response.body);
-    // print(data);
+  String time = 'loading';
 
-    // get properties from data
-    String datetime = data['datetime'];
-    String offset = data['utc_offset'].substring(1,3);
-    // print(datetime);
-    // print(offset);
-
-    //create a datetime object
-    DateTime now = DateTime.parse(datetime);
-    now = now.add(Duration(hours: int.parse(offset)));
-    print(now);
+  void setupWorldTime() async {
+    WorldTime instance = WorldTime(location: 'Karachi', flag: '', url: 'Asia/Karachi');
+    await instance.getTime();
+    print(instance.time);
+    setState(() {
+      time = instance.time;
+    });
   }
 
   @override
   void initState() {
     super.initState();
-    getTime();
+    setupWorldTime();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('loading screen'),
+      body: Padding(
+        padding: EdgeInsets.all(50.0),
+        child: Text(time),
+      ),
     );
   }
 }
